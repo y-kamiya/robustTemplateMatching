4f578f168e9f7dcc0f615f198e73b24102a76e1d
Yuji Kamiya <y.kamiya0@gmail.com>
2020-07-01 20:07:32 +0900

    improve log

--- run.py
+++ run.py
@@ -15,8 +15,6 @@ import sys
 import logging
 
 class Evaluator:
-    SCORE_THRESHOLD = 0.3
-
     def __init__(self, config):
         self.config = config
 
@@ -60,18 +58,22 @@ class Evaluator:
 
         return name.split('@')[:-1]
 
-    def get_matched_templates(self, score_map, n_top):
+    def get_matched_templates(self, score_map, n_top, image_path):
         entry_all = []
-        for template_path, entry in score_map.items():
+        for name, entry in score_map.items():
             for i in range(len(entry[0])):
-                entry_all.append((template_path, entry[0][i], entry[1][i]))
+                entry_all.append((name, entry[0][i], entry[1][i]))
 
-        sorted_entries = sorted(entry_all, key=lambda x:-x[2])[:n_top]
+        entries = sorted(entry_all, key=lambda x:-x[2])
+        self.config.logger.info('sorted list: {}\t{}'.format(os.path.basename(image_path), entries[:5]))
+
+        sorted_entries = entries[:n_top]
+
         if len(sorted_entries) <= 0:
             sorted_entries.append(('none.png', [[0,0],[0,0]], [0.0]))
 
         return sorted_entries
-        # labels = self.extract_labels(template_path, True)
+        # labels = self.extract_labels(name, True)
         # return {'label': labels[0], 'boxes': entry[0], 'scores': entry[2]}
 
     def output_result(self, result, is_log=True):
@@ -122,6 +124,8 @@ class Evaluator:
             if self.is_img(path):
                 template_paths.append(os.path.join(self.config.template_dir, path))
 
+        image_paths.sort()
+        template_paths.sort()
         self.config.logger.debug(image_paths)
         self.config.logger.debug(template_paths)
 
@@ -130,6 +134,7 @@ class Evaluator:
             score_map = {}
             raw_image = cv2.imread(image_path)[..., ::-1]
             image = image_transform(raw_image.copy()).unsqueeze(0)
+            image_name = os.path.basename(image_path)
 
             for template_path in template_paths:
                 start_time = time.time()
@@ -143,29 +148,29 @@ class Evaluator:
                 # self.config.logger.debug("detected objects: {}".format(len(indexes)))
                 # score_map[template_path] = [boxes[indexes], scores[indexes]]
 
-                if self.SCORE_THRESHOLD <= scores[0]:
-                    score_map[template_path] = [boxes, scores]
+                if self.config.score_threshold <= scores[0]:
+                    score_map[os.path.basename(template_path)] = [boxes, scores]
 
-                self.config.logger.info('{:.2f}\t{:.4}\t{}\t{}'.format(time.time() - start_time, scores[0], template_path, image_path))
+                self.config.logger.info('{:.2f}\t{:.4}\t{}\t{}'.format(time.time() - start_time, scores[0], template_path, image_name))
 
             FE.remove_cache(image_path)
 
-            matched_entries = self.get_matched_templates(score_map, 2)
-            self.config.logger.debug('{} matches {}'.format(image_path, matched_entries))
+            matched_entries = self.get_matched_templates(score_map, 2, image_name)
+            self.config.logger.debug('{} matches {}'.format(image_name, matched_entries))
 
             if len(matched_entries) == 0:
                 continue
 
-            result[image_path] = []
+            result[image_name] = []
             d_img = raw_image.astype(np.uint8).copy()
             for entry in matched_entries:
                 box = entry[1]
                 d_img = cv2.rectangle(d_img, (box[0][0],box[0][1]), (box[1][0],box[1][1]), (255, 0, 0), 3)
 
-                result[image_path].append(entry[0])
+                result[image_name].append(entry[0])
                 
-            cv2.imwrite(os.path.join(self.config.output_dir, os.path.basename(image_path)), d_img[..., ::-1])
-            self.config.logger.info('result: {}\t{}'.format(image_path, result[image_path]))
+            cv2.imwrite(os.path.join(self.config.output_dir, image_name), d_img[..., ::-1])
+            self.config.logger.info('result: {}\t{}'.format(image_name, result[image_name]))
             
 
         self.output_result(result)
@@ -179,6 +184,7 @@ if __name__ == '__main__':
     parser.add_argument('--cpu', action='store_true')
     parser.add_argument('--loglevel', default='INFO')
     parser.add_argument('--logfile', default=None)
+    parser.add_argument('--score_threshold', type=float, default=0.3)
     args = parser.parse_args()
 
     is_cpu = args.cpu or not torch.cuda.is_available()
@@ -193,6 +199,8 @@ if __name__ == '__main__':
         handler = logging.FileHandler(args.logfile)
         handler.setLevel(getattr(logging, args.loglevel))
         logger.addHandler(handler)
+
+    logger.info(args)
 
     evaluator = Evaluator(args)
     evaluator.execute()
